8d471a3f9245f2dc12d6a66f125880c45715bffd
liranran <921090271@qq.com>
2016-11-12 12:33:29 +0800

    problem 4-3

---
+++ tree.h
@@ -0,0 +1,71 @@
+/*************************************************************************
+	> File Name: tree.h
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: 2016年11月10日 星期四 18时50分38秒
+ ************************************************************************/
+//本文件中lChild代表son，rChild代表brother
+typedef char elementType;
+typedef	struct Tree{
+	elementType data;
+	struct Tree *lChild,*rChild;
+} BiNode,*Bitree;
+//create Subtree
+void  createSubTree(Bitree q,int k)
+{
+	//q->root node
+	//k=1 --left subtree,k=2 --right subtree
+	elementType x;
+	Bitree u;
+	cin>>x;
+	if(x!='#')
+	{
+		u = new BiNode;
+		u -> data = x;
+		u -> lChild = NULL;
+		u -> rChild = NULL;
+		if(k==1)
+			q->lChild = u;
+		if(k==2)
+			q->rChild = u;
+		createSubTree(u,1);
+		createSubTree(u,2);
+	}
+}
+//create binary tree
+void createBiTree(Bitree &BT)
+{
+	Bitree p;
+	char x;
+	cout<<"please according to preorder traversal enter binary tree ,end with '#':"<<endl;
+	cin>>x;
+	if(x=='#')
+		return;
+	BT = new BiNode;
+	BT->data=x;
+	BT->lChild=NULL;
+	BT->rChild=NULL;
+	p=BT;
+	createSubTree(p,1); //create root node left subtree
+	createSubTree(p,2); //create root node right subtree
+}
+void Visit(Bitree T)
+{
+	if(T -> data != '#')
+		cout<<T->data;
+}
+//
+//
+void PreOrder(Bitree T)
+{
+	if(T)
+	{
+		Visit(T); 
+		PreOrder(T->lChild);
+		PreOrder(T->rChild);
+	}
+}
+//
+//
+//
+
