48cab8d30ecb2b70e45605bd99b9f76f6d9c6834
lrr <921090271@qq.com>
2016-11-29 23:34:29 +0800

    二分查找插入

--- lookup_4_1.cpp
+++ lookup_4_1.cpp
@@ -19,6 +19,38 @@ int seq_search(seqList L,ElemType key)
     return i;
 
 }
+/*
+ * 利用二分法在一个有序表中
+ * 插入一个元素x，
+ * 并保持表的有序性
+ */
+void bin_search(seqList &L,ElemType x)
+{
+	int mid,low=0,high=L.listLen-1,loc=-1;
+	while(low <= high)
+	{
+		mid = (low + high)/2;
+		if( x > L.data[mid-1] && x <= L.data[mid])
+		{
+			loc = mid;
+			break;
+		} else if(x < L.data[mid-1])
+		{
+			high = mid-1;
+
+		}else
+		  low = mid+1;
+	}
+	if(loc!=-1)
+	{
+		for(int i=L.listLen-1; i >= mid ;i--)
+		  L.data[i+1]=L.data[i];
+		L.data[mid]=x;
+		L.listLen++;
+		cout<<"\n<--insert success-->\n";
+	}else
+	  cout<<"\n error \n";
+}
 int main()
 {
 	seqList Q;
@@ -26,8 +58,10 @@ int main()
 	initialSeqList(Q);
 	createSeqList(Q);
 	printSeqList(Q);
+	bin_search(Q,4);
+	printSeqList(Q);
 	//x=seq_search(Q,2);
 	//cout<<"x-->"<<x<<endl;
-	cout<<"find result: "<<seq_search(Q,2)<<endl;
+//	cout<<"find result: "<<seq_search(Q,2)<<endl;
 	return 0;
 }
